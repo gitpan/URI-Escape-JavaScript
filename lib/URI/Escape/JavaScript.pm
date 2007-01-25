c5bb21084dbf99c1795fcaee1c3ad3f8d4de3f62
Koichi Taniguchi <taniguchi@cpan.org>
2007-01-25 02:29:01 +0000

    Import of TANIGUCHI/URI-Escape-JavaScript-0.02 from CPAN.
    gitpan-cpan-distribution: URI-Escape-JavaScript
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         TANIGUCHI/URI-Escape-JavaScript-0.02.tar.gz
    gitpan-cpan-author:       TANIGUCHI
    gitpan-cpan-maturity:     released

--- lib/URI/Escape/JavaScript.pm
+++ lib/URI/Escape/JavaScript.pm
@@ -3,9 +3,10 @@ package URI::Escape::JavaScript;
 use strict;
 use 5.8.1;
 use warnings;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 use Encode qw(encode FB_PERLQQ);
 use base qw(Exporter);
+our @EXPORT    = qw(js_escape js_unescape);
 our @EXPORT_OK = qw(escape unescape);
 
 sub escape {
@@ -22,6 +23,9 @@ sub unescape {
     $escaped =~ s/%([0-9a-f]{2})/chr(hex($1))/eig;
     return $escaped;
 }
+
+*js_escape   = \&escape;
+*js_unescape = \&unescape;
 
 1;
 __END__
@@ -40,6 +44,14 @@ URI::Escape::JavaScript - A perl implementation of JavaScript's escape() and une
  $escaped = '%u30B5%u30D6%u30C6%u30AF%u5165%u308A%u305F%u3044%uFF01%uFF01';
  $string  = unescape($escaped);
 
+ use URI::Escape::JavaScript;
+ 
+ $string  = "\x{30c9}\x{30f3}\x{5f15}\x{304d}\x{3057}\x{305f}";
+ $escaped = js_escape($string);
+ 
+ $escaped = '%u3059%u305A%u304D%u308A%u3093%u305F%u308D%u3046';
+ $string  = js_unescape($escaped);
+
 =head1 DESCRIPTION
 
 URI::Escape::JavaScript provides JavaScript's C<escape()> and C<unescape()> functions. It works simplar to homonymous functions of JavaScript.
@@ -49,13 +61,30 @@ L<URI::Escape> doesn't work for escaping and unescaping JavaScript like Unicode 
 
 =head2 escape()
 
-C<escape()> works to escaping a string to JavaScript's URI-escaped string. The argument of this function must be a flagged UTF-8 string (a.k.a. Perl's internal form).
-This function is exportable (but will not be exported by default). C<use> it with arguments of function names if you want.
+C<escape()> works to escape a string to JavaScript's URI-escaped string.
+The argument of this function must be a flagged UTF-8 string
+(a.k.a. Perl's internal form).
+This function is exportable (but will not be exported by default).
+C<use> it with arguments of function names if you want
+(It's for backward compatibility).
 
 =head2 unescape()
 
-C<unescape()> works to unescaping JavaScript's URI-escaped string to original string. It will return a flagged UTF-8 string (a.k.a. Perl's internal form).
-This function is also esportable (but will not be exported by default). C<use> it with arguments of function names if you want.
+C<unescape()> works to unescape JavaScript's URI-escaped string to original
+string. It will return a flagged UTF-8 string (a.k.a. Perl's internal form).
+This function is also exportable (but will not be exported by default).
+C<use> it with arguments of function names if you want
+(It's also for backward compatibility).
+
+=head2 js_escape()
+
+It's a synonym for C<escape()>. This function will be exported on your
+namespace.
+
+=head2 js_unescape()
+
+It's a synonym for C<unescape()>. This function will be exported on your
+namespace.
 
 =head1 AUTHOR
 
@@ -68,6 +97,6 @@ it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
-L<URI::Escape>, L<Encode>
+L<URI::Escape>, L<Encode>, L<Encode::JavaScript::UCS>
 
 =cut
